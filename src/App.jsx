afb311d3efd7d996cd30cc15a4ec9cb3e2b997ae
이시형 <tlgud585@gmail.com>
2022-10-04 05:21:01 +0900

    Update : discover page 카테고리 리스트 파일 정의, song card 공통 컴포넌트 생성,

--- src/App.jsx
+++ src/App.jsx
@@ -9,17 +9,21 @@ import Search from './pages/Search';
 
 function App() {
   return (
-    <>
-      <Routes>
-        <Route path="/" element={<Discover />} />
-        <Route path="/top-artists" element={<TopArtists />} />
-        <Route path="/top-charts" element={<TopCharts />} />
-        <Route path="/around-you" element={<AroundYou />} />
-        <Route path="/artists/:id" element={<ArtistDetails />} />
-        <Route path="/songs/:songid" element={<SongDetails />} />
-        <Route path="/search/:searchTerm" element={<Search />} />
-      </Routes>
-    </>
+    <div className="flex-1 flex flex-col bg-gradient-to-br from-black to-[#121286]">
+      <div className="px-6 h-[calc(100vh-72px)] overflow-y-scroll hide-scrollbar flex xl:flex-row flex-col-reverse">
+        <div className="flex-1 h-fit pb-40">
+          <Routes>
+            <Route path="/" element={<Discover />} />
+            <Route path="/top-artists" element={<TopArtists />} />
+            <Route path="/top-charts" element={<TopCharts />} />
+            <Route path="/around-you" element={<AroundYou />} />
+            <Route path="/artists/:id" element={<ArtistDetails />} />
+            <Route path="/songs/:songid" element={<SongDetails />} />
+            <Route path="/search/:searchTerm" element={<Search />} />
+          </Routes>
+        </div>
+      </div>
+    </div>
   );
 }
 
--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -1,7 +1,52 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { genres } from '../assets/constants';
+import Loader from '../components/Loader';
+import Error from '../components/Error';
+
+import SongCard from '../components/SongCard';
+import { useGetTopChartsQuery } from '../redux/services/shazamCore';
+import { selectGenreListId } from '../redux/features/playerSlice';
 
 const Discover = () => {
-  return <div>Discover</div>;
+  const dispatch = useDispatch();
+  const { activeSong, isPlaying } = useSelector(state => state.player);
+  const { data, isFetching, error } = useGetTopChartsQuery();
+  const genreTitle = 'Pop';
+
+  if (isFetching) return <Loader title="Loading songs..." />;
+  if (error) return <Error />;
+
+  return (
+    <div className="flex flex-col">
+      <div className="flex flex-col justify-between items-center w-full mt-4 mb-10 sm:flex-row">
+        <h2 className="font-bold text-3xl text-white text-left">Discover {genreTitle}</h2>
+        <select
+          onChange={e => dispatch(selectGenreListId(e.target.value))}
+          value=""
+          className="bg-black text-gray-300 text-sm p-3 mt-5 rounded-lg outline-none sm:mt-0"
+        >
+          {genres.map(({ title, value }) => (
+            <option key={value} value={value}>
+              {title}
+            </option>
+          ))}
+        </select>
+      </div>
+      <div className="flex flex-wrap justify-center gap-8 sm:justify-start">
+        {data.map((song, i) => (
+          <SongCard
+            key={song.key}
+            isPlaying={isPlaying}
+            activeSong={activeSong}
+            song={song}
+            data={data}
+            i={i}
+          />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Discover;
