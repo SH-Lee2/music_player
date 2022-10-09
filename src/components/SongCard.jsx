8ab474efac380d34164dee0c3d932db37f4e9406
이시형 <tlgud585@gmail.com>
2022-10-10 08:21:28 +0900

    Style : reponsive

--- src/components/SongCard.jsx
+++ src/components/SongCard.jsx
@@ -18,7 +18,7 @@ const SongCard = ({ song, data, i, isPlaying, activeSong }) => {
   };
 
   return (
-    <div className="flex flex-col w-[250px] p-4 bg-white/5 bg-opacity-80 backdrop-blur-sm animate-slideup rounded-lg cursor-pointer">
+    <div className="flex flex-col w-full max-w-[350px] p-4 bg-white/5 bg-opacity-80 backdrop-blur-sm animate-slideup rounded-lg cursor-pointer sm:w-[250px]">
       <div className="relative w-full h-56 group">
         <div
           className={`absolute inset-0 justify-center items-center bg-black bg-opacity-50 group-hover:flex ${
